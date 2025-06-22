b781e08bcff42bcb33c8f2c15996673eb10f8ac6
elefsariasama <agusrurisumardi88@gmail.com>
2025-06-23 02:51:09 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pink: {
+          500: "#db2777"
+        }
+      }
+    },
   },
   plugins: [],
 }
